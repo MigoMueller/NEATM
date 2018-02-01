19ff397ec2ef3265bd006c337c6b8fef656c83f5
MigoMueller <m.mueller@astro.rug.nl>
2018-02-01 13:52:45 +0100

    Python wrapper: improved handling of array of wavelengths

--- neatm.py
+++ neatm.py
@@ -1,7 +1,8 @@
 # M.Mueller@astro.rug.nl, 2016/09/05
 # Wrapper around my NEATM executable.
 # Output will be in mJy (default) or W/m^2/micron.
-# LambdaMu can be an array or scalar; output will be np.array of floats or a single float.
+# LambdaMu can be an array (iterable) or a scalar Quantity;
+# output will be a list of Quantitites or a single Quantity, resp.
 
 from __future__ import print_function
 
@@ -10,13 +11,7 @@ def neatm(h,g,alpha,r,delta,lambdaMu,eta,pv, mJy=True):
     import numpy as np
     from astropy import units as u
     try:
-        result=np.empty(len(lambdaMu)) 
-        for i,wavelength in enumerate(lambdaMu):
-            dummy=neatm(h,g,alpha,r,delta,wavelength,eta,pv,mJy=mJy)
-            result[i]=dummy.value
-            unit=dummy.unit
-        return result*unit
-        #return np.array([neatm(h,g,alpha,r,delta,wav,eta,pv,mJy=mJy) for wav in lambdaMu])  ## doesn't work nicely with units...
+        return [neatm(h,g,alpha,r,delta,wav,eta,pv,mJy=mJy) for wav in lambdaMu]  
     except TypeError:
         pass # TypeError will be thrown if lambdaMu is a scalar; ignore and carry on
     try:
